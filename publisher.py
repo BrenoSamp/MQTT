2ea96e14225486f9db551bb9fedb1a229f0d2b7b
Breno_codes <breno.santos@mova.vc>
2024-03-24 21:53:58 -0300

    feat: add django

--- publisher.py
+++
@@ -1,6 +0,0 @@
-import paho.mqtt_client as mqtt
-from application.configs.broker_configs import mqtt_broker_configs
-
-mqtt_client = mqtt.Client('publisher')
-mqtt_client.connect(host=mqtt_broker_configs["HOST"], port=mqtt_broker_configs["PORT"])
-mqtt_client.publish(topic='', payload='{}')
