e442e2385c0e42c437a1af25d99471c28e7c4578
Breno_codes <breno.santos@mova.vc>
2024-03-27 20:19:34 -0300

    fix: ajuste

---
+++ sendText.py
@@ -0,0 +1,15 @@
+from main.subscribers.subscriber import Subscriber
+from main.publishers.publisher import Publisher
+
+def textEditor():
+    name = input("Digite seu nome: ")
+    Publisher.sendToTextEditorTopic(name)
+
+print("0 - Finalizar\n1 - Editar texto\n2 - Editar arquivo\n3- Realizar calculo\n")
+opcao = int(input('Digite a opção: '))
+while(opcao != 0):
+    print("0 - Finalizar\n1 - Editar texto\n2 - Editar arquivo\n3- Realizar calculo\n")
+    opcao = int(input('Digite a opção: '))
+    if(opcao == 1):
+        textEditor()
+print('Até mais camarada!')
