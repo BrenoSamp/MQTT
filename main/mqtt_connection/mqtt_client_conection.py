6df52705cde0f879059549bf149a33ae9146344a
Breno_codes <breno.santos@mova.vc>
2024-04-15 20:09:04 -0300

    fix: ajustes mqtt

--- main/mqtt_connection/mqtt_client_conection.py
+++ main/mqtt_connection/mqtt_client_conection.py
@@ -7,8 +7,7 @@ class MqttClientConnection:
         self.__client_name = client_name
         self.__keepalive = keepalive
 
-    def start_connection(self):
-        mqtt_client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION1, self.__client_name)
+    def start_connection(self, mqtt_client):
         mqtt_client.connect(host=self.__broker_ip, port=self.__port, keepalive=self.__keepalive)
         self.__mqtt_client = mqtt_client
         return mqtt_client
--- main/publishers/publisher.py
+++ main/publishers/publisher.py
@@ -1,18 +1,20 @@
 from main.configs.broker_configs import mqtt_broker_configs
 from main.mqtt_connection.callbacks import on_connect_text_editor_response, on_message_text_editor_response, on_subscribe_text_editor_response, on_connect_file_editor_response, on_message_file_editor_response, on_subscribe_file_editor_response, on_connect_calculate_response, on_message_calculate_response, on_subscribe_calculate_response
 from main.mqtt_connection.mqtt_client_conection import MqttClientConnection
+import paho.mqtt.client as mqtt
 import time
 
 class Publisher:
     ## TEXT EDITOR METHODS
     def sendToTextEditorTopic(message: str):
-        mqtt_client = MqttClientConnection(
-            mqtt_broker_configs["HOST"], mqtt_broker_configs["PORT"], 'text_editor_publisher', mqtt_broker_configs["KEEPALIVE"])
-
+        mqttConection = MqttClientConnection(
+        mqtt_broker_configs["HOST"], mqtt_broker_configs["PORT"], 'text_editor_publisher', mqtt_broker_configs["KEEPALIVE"])
+        mqtt_client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION1, 'text_editor_publisher')
         mqtt_client.on_connect = on_connect_text_editor_response
         mqtt_client.on_message = on_message_text_editor_response
         mqtt_client.on_subscribe = on_subscribe_text_editor_response
-        mqtt_client.start_connection()
+
+        mqttConection.start_connection(mqtt_client)
         mqtt_client.publish(topic=mqtt_broker_configs['TEXT_EDITOR_TOPIC'], payload=message)
         mqtt_client.loop()
 
